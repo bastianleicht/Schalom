bbbebf0962ad4bd7c506f487f2e3785f9bd3cac7
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-01 18:27:42 +0200

    UPDATE: Code Formatting

--- commands/public/music/queue.js
+++ commands/public/music/queue.js
@@ -8,24 +8,25 @@ const { MessageEmbed } = require("discord.js");
 const sendError = require("../../../utils/error");
 
 module.exports.run = async function (client, message, args) {
-  if (message.channel === 'dm') return;
 
-  const serverQueue = message.client.queue.get(message.guild.id);
-  if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+    if (message.channel === 'dm') return;
 
-  let queue = new MessageEmbed()
-  .setAuthor("Server Songs Queue", "https://cdn.bastianleicht.de/etc/schalom/music.gif")
-  .setColor("BLUE")
-  .addField("Now Playing", serverQueue.songs[0].title, true)
-  .addField("Text Channel", serverQueue.textChannel, true)
-  .addField("Voice Channel", serverQueue.voiceChannel, true)
-  .setDescription(serverQueue.songs.map((song) => {
-    if(song === serverQueue.songs[0])return;
-    return `**-** ${song.title}`;
-  }).join("\n"))
-  .setFooter(client.config.copyright + " Currently Server Volume is "+serverQueue.volume);
-  if(serverQueue.songs.length === 1)queue.setDescription(`No songs to play next add songs by \`\`${client.config.prefix}play <song_name>\`\``);
-  message.channel.send(queue);
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+
+    let queue = new MessageEmbed()
+    .setAuthor("Server Songs Queue", "https://cdn.bastianleicht.de/etc/schalom/music.gif")
+    .setColor("BLUE")
+    .addField("Now Playing", serverQueue.songs[0].title, true)
+    .addField("Text Channel", serverQueue.textChannel, true)
+    .addField("Voice Channel", serverQueue.voiceChannel, true)
+    .setDescription(serverQueue.songs.map((song) => {
+      if(song === serverQueue.songs[0])return;
+      return `**-** ${song.title}`;
+    }).join("\n"))
+    .setFooter(client.config.copyright + " Currently Server Volume is "+serverQueue.volume);
+    if(serverQueue.songs.length === 1)queue.setDescription(`No songs to play next add songs by \`\`${client.config.prefix}play <song_name>\`\``);
+    message.channel.send(queue);
 };
 
 module.exports.help = {
