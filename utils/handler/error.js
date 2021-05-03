925ab0c6dc7becefd97b39aea855f6bdf4185a23
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-03 19:26:02 +0200

    Update: Added Date

--- utils/handler/error.js
+++ utils/handler/error.js
@@ -4,27 +4,42 @@
  * PDX-License-Identifier: BSD-2-Clause
  */
 
+function getDate() {
+    let date = new Date();
+    let year = date.getYear() + 1900;
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[date.getMonth()];
+    let day = date.getDate().toString().length < 2 ? "0" + date.getDate() : date.getDate();
+    let hour = date.getHours().toString().length < 2 ? "0" + date.getHours() : date.getHours();
+    let minute = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes() : date.getMinutes();
+    let second = date.getSeconds().toString().length < 2 ? "0" + date.getSeconds() : date.getSeconds();
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+}
+
 module.exports = {
     disconnect: () => {
-        console.warn('Disconnected!');
+        console.warn(getDate() + ' | Disconnected!');
     },
     reconnecting: () => {
-        console.log("Bot reconnecting...");
+        console.log(getDate() + " | Bot reconnecting...");
     },
     warn: (warning) => {
-        console.warn('[WARNING]', warning);
+        console.warn(getDate() + ' | [WARNING]', warning);
     },
     error: (error) => {
-        console.error(`ERROR: ${error.message}`);
+        console.error(`${getDate()} | ERROR: ${error.message}`);
+    },
+    mysql: (error) => {
+        console.log(getDate() + ' | [MYSQL] ERROR:', error);
     },
     DiscordAPIError: (error) => {
-        console.log('[DiscordAPIError]', error);
+        console.log(getDate() + ' | [DiscordAPIError]', error);
     },
     uncaughtException: (error) => {
-        console.error(`[uncaughtException] ${error.stack}`);
+        console.error(`${getDate()} | [uncaughtException] ${error.stack}`);
         process.exit(1);
     },
     unhandledRejection: (error, promise) => {
-        console.log('[unhandledRejection]', `Reason: ${error.stack}`);
+        console.log(getDate() + ' | [unhandledRejection]', `Reason: ${error.stack}`);
     },
 };
