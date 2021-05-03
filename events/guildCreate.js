d99dfb36632e9a6b98e300ca0d3feb72a32a273c
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-03 16:00:43 +0200

    Added: guildCreate Event

---
+++ events/guildCreate.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020-2021, Bastian Leicht <mail@bastianleicht.de>
+ *
+ * PDX-License-Identifier: BSD-2-Clause
+ */
+
+module.exports = async (client, guild) => {
+    try {
+        client.db.query('SELECT * FROM server WHERE guildID = ?', [guild.id], (err, { length }) => {
+            if(err) return console.error(`MYSQL: query guildID ERROR: ${err}`);
+            if(length) {
+                //  Guild already exists in DB
+                console.log(`${client.user.username} just re-joined a guild! \nGuild Name: '${guild.name}' \nGuild ID: '${guild.id}' \nGuild Membercount: '${guild.memberCount}'!`);
+
+                guild.owner.user.send(`***Hey there! Thanks for re-inviting ${client.user.username} to your server. The default prefix is "${client.config.prefix}".`)
+                    .catch((err) => console.error('Failed to dm guild owner! ERROR: ' + err));
+            } else {
+                //  Guild is not in DB
+                try {
+                    client.db.query('INSERT INTO server ( guildID ) values (?)', [guild.id], (err, { insertId }) => {
+
+                        if (err) return console.error(`MYSQL: guildCreate (id: ${guild.id}) insertion failed! Error: ${err}`);
+                        if (insertId === null) return console.error(`MYSQL: guildCreate (id: ${guild.id}) insertion failed!`);
+
+                        console.log(`${client.user.username} just joined a new guild! \nGuild Name: '${guild.name}' \nGuild ID: '${guild.id}' \nGuild Membercount: '${guild.memberCount}'!`);
+
+                        guild.owner.user.send(`***Hey there! Thanks for inviting ${client.user.username} to your server. The default prefix is "${client.config.prefix}".`)
+                            .catch((err) => console.error('Failed to dm guild owner! ERROR: ' + err));
+                    });
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        });
+    } catch (e) {
+        console.error(`MYSQL: ERROR: ${e}`);
+    }
+
+};
