bb81e564ef38c865ce2406998f0862e99257164c
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:09:59 +0200

    Economy: Updated profile Command to MySQL

--- commands/public/economy/profile.js
+++ commands/public/economy/profile.js
@@ -3,39 +3,64 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const Discord = require("discord.js");
-const db = require("quick.db");
+const Discord       = require("discord.js");
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
 module.exports.run = async (client, message, args) => {
 
-    let user = message.mentions.members.first() || message.author;
+    const user = message.mentions.members.first() || message.author;
 
-    let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
-    if (money === null) money = 0;
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [user.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${user.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${user.id}"!`);
 
-    let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`);
-    if (bank === null) bank = 0;
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${user.id}"!\n ${error}`);
 
-    let vip = await db.fetch(`bronze_${message.guild.id}_${user.id}`);
-    if(vip === null) vip = 'None';
-    if(vip === true) vip = 'Bronze';
+                    const money = rows[0].money;
+                    const bank = rows[0].bank;
+                    const house = rows[0].house;
+                    const car = rows[0].car;
+                    const rank = rows[0].rank;
 
-    let shoes = await db.fetch(`nikes_${message.guild.id}_${user.id}`);
-    if(shoes === null) shoes = '0';
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`**__${user}'s Profile:__**\nPocket: ${money}\nBank: ${bank}\nRank: ${rank}\n\n**Inventory:**\nCars: ${car}\nMansions: ${house}`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+                });
+            });
+        } else {
+            // User exists in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [user.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${user.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${user.id}"!`);
 
-    let newcar = await db.fetch(`car_${message.guild.id}_${user.id}`);
-    if(newcar === null) newcar = '0';
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${user.id}"!\n ${error}`);
 
-    let newhouse = await db.fetch(`house_${message.guild.id}_${user.id}`);
-    if(newhouse === null) newhouse = '0';
+                    const money = rows[0].money;
+                    const bank = rows[0].bank;
+                    const house = rows[0].house;
+                    const car = rows[0].car;
+                    const rank = rows[0].rank;
 
-    let moneyEmbed = new Discord.MessageEmbed()
-        .setTitle('💰 Economy')
-        .setDescription(`**${user}'s Profile**\n\nPocket: ${money}\nBank: ${bank}\nVIP Rank: ${vip}\n\n**Inventory**\n\nNikes: ${shoes}\nCars: ${newcar}\nMansions: ${newhouse}`)
-        .setColor(0x8e44ad)
-        .setTimestamp()
-        .setFooter(client.config.copyright);
-    message.channel.send(moneyEmbed);
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`**__${user}'s Profile:__**\nPocket: ${money}\nBank: ${bank}\nRank: ${rank}\n\n**Inventory:**\nCars: ${car}\nMansions: ${house}`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+                });
+            });
+        }
+    });
 };
 
 module.exports.help = {
