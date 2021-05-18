c1ba57b560c31f96b16c3cf836f80b6e8c2ed88d
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:17:08 +0200

    Economy: Added updateBank() and errorEmbed()

--- utils/helper/economyHelper.js
+++ utils/helper/economyHelper.js
@@ -3,35 +3,24 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const errorHandler = require('./../handler/error');
-
-function checkUser(user_id, connection) {
-    try {
-        connection.query('SELECT * FROM economy WHERE userID = ?', [user_id], (error, { length }) => {
-            if(error) return errorHandler.mysql(`Error while querying for User : "${user_id}"!\n ${error}`);
-            if(!length) {
-                // User does not exist in DB
-                try {
-                    connection.query(`INSERT INTO economy ( userid ) values (?)`, [user_id], (error, { insertId }) => {
-                        if(error) return errorHandler.mysql(`Error while inserting User : "${user_id}"!\n ${error}`);
-                        if(!insertId) return errorHandler.mysql(`Error while inserting User : "${user_id}"!`);
-                    });
-                } catch (error) {
-                    return errorHandler.error(error);
-
-                }
-            }
-        });
-    } catch (error) {
-        return errorHandler.error(error);
-    }
-}
+const Discord = require('discord.js');
+const errorHandler = require(__BASE__ + '/utils/handler/error');
 
 function updateMoney(user_id, amount, connection) {
     try {
         connection.query('UPDATE economy SET money = ? WHERE userID = ?', [amount, user_id], (error) => {
-            if(error) return errorHandler.mysql(`Error while updating money to '${amount}' for User : "${user_id}"!\n ${error}`);
+            if (error) return errorHandler.mysql(`Error while updating money to '${amount}' for User : "${user_id}"!\n ${error}`);
 
+        });
+    } catch (error) {
+        errorHandler.error(error);
+    }
+}
+
+function updateBank(user_id, amount, connection) {
+    try {
+        connection.query('UPDATE economy SET bank = ? WHERE userID = ?', [amount, user_id], (error) => {
+            if (error) return errorHandler.mysql(`Error while updating bank to '${amount}' for User : "${user_id}"!\n ${error}`);
 
         });
     } catch (error) {
@@ -42,7 +31,7 @@ function updateMoney(user_id, amount, connection) {
 function setValue(user_id, value, value_to, connection) {
     try {
         connection.query(`UPDATE economy SET ${value} = ? WHERE userID = ?`, [value_to, user_id], (error) => {
-            if(error) return errorHandler.mysql(`Error while updating '${value}' to '${value_to}' for User : "${user_id}"!\n ${error}`);
+            if (error) return errorHandler.mysql(`Error while updating '${value}' to '${value_to}' for User : "${user_id}"!\n ${error}`);
 
         });
     } catch (e) {
@@ -50,8 +39,19 @@ function setValue(user_id, value, value_to, connection) {
     }
 }
 
+function errorEmbed() {
+    let embed = new Discord.MessageEmbed()
+        .setTitle('💰 Economy | Error')
+        .setDescription(`Seems like there was a Database error!`)
+        .setColor(0x8e44ad)
+        .setTimestamp();
+
+    return embed;
+}
+
 module.exports = {
-    checkUser,
     updateMoney,
+    updateBank,
     setValue,
+    errorEmbed,
 };
