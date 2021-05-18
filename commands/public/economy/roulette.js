c133ac724fff9660d905af27b0b5e6a3e29f2322
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:53:28 +0200

    Economy: Updated roulette to MySQL

--- commands/public/economy/roulette.js
+++ commands/public/economy/roulette.js
@@ -3,104 +3,223 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const Discord = require("discord.js");
-const db = require("quick.db");
+const Discord       = require("discord.js");
+const economyHelper = require(__BASE__ + '/utils/helper/economyHelper');
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
 module.exports.run = async (client, message, args) => {
 
-    let user = message.author;
+    let color = args[0];
+    let amount = parseInt(args[1]);
+    const random = Math.floor(Math.random() * 37);
+    color = color.toLowerCase();
 
     function isOdd(num) {
         if ((num % 2) === 0) return false;
         else if ((num % 2) === 1) return true;
     }
 
-    let colour = args[0];
-    let money = parseInt(args[1]);
-    let moneydb = await db.fetch(`money_${message.guild.id}_${user.id}`);
-
-    let random = Math.floor(Math.random() * 37);
-
-    let moneyhelp = new Discord.MessageEmbed()
-        .setTitle('💰 Economy')
-        .setDescription(`❌ Specify an amount to gamble | ${client.config.prefix}roulette <color> <amount>`)
-        .setColor("#FF0000")
-        .setTimestamp()
-        .setFooter(client.config.copyright);
-
-    let moneymore = new Discord.MessageEmbed()
-        .setTitle('💰 Economy')
-        .setDescription(`❌ You are betting more than you have`)
-        .setColor("#FF0000")
-        .setTimestamp()
-        .setFooter(client.config.copyright);
-
-    let bad_color = new Discord.MessageEmbed()
-        .setTitle('💰 Economy')
-        .setDescription(`❌ Specify a color | Red [1.5x] Black [2x] Green [15x]`)
-        .setColor("#FF0000")
-        .setTimestamp()
-        .setFooter(client.config.copyright);
-
-    if (!colour)  return message.channel.send(bad_color);
-    colour = colour.toLowerCase();
-    if (!money) return message.channel.send(moneyhelp);
-    if (money > moneydb) return message.channel.send(moneymore);
-
-    if (colour === "b" || colour.includes("black")) colour = 0;
-    else if (colour === "r" || colour.includes("red")) colour = 1;
-    else if (colour === "g" || colour.includes("green")) colour = 2;
-    else return message.channel.send(bad_color);
-
-    if (random === 0 && colour === 2) { // Green
-        money *= 15;
-        db.add(`money_${message.guild.id}_${user.id}`, money);
-
-        let moneyEmbed1 = new Discord.MessageEmbed()
+    if (!color) {
+        let embed = new Discord.MessageEmbed()
             .setTitle('💰 Economy')
-            .setDescription(`🟩 You won ${money} coins\n\nMultiplier: 15x`)
-            .setColor(0x8e44ad)
+            .setDescription(`❌ Specify a color | Red [1.5x] Black [2x] Green [15x]`)
+            .setColor("#FF0000")
             .setTimestamp()
             .setFooter(client.config.copyright);
-
-        message.channel.send(moneyEmbed1);
-        console.log(`${message.author.tag} Won ${money} on green`);
-    } else if (isOdd(random) && colour === 1) { // Red
-
-        money = parseInt(money * 1.5);
-        db.add(`money_${message.guild.id}_${user.id}`, money);
-
-        let moneyEmbed2 = new Discord.MessageEmbed()
-            .setTitle('💰 Economy')
-            .setDescription(`🟥 You won ${money} coins\n\nMultiplier: 1.5x`)
-            .setColor(0x8e44ad)
-            .setTimestamp()
-            .setFooter(client.config.copyright);
-        message.channel.send(moneyEmbed2);
-    } else if (!isOdd(random) && colour === 0) { // Black
-
-        money = parseInt(money * 2);
-        db.add(`money_${message.guild.id}_${user.id}`, money);
-
-        let moneyEmbed3 = new Discord.MessageEmbed()
-            .setTitle('💰 Economy')
-            .setDescription(`⬛ You won ${money} coins\n\nMultiplier: 2x`)
-            .setColor(0x8e44ad)
-            .setTimestamp()
-            .setFooter(client.config.copyright);
-        message.channel.send(moneyEmbed3);
-    } else { // Wrong
-
-        db.subtract(`money_${message.guild.id}_${user.id}`, money);
-
-        let moneyEmbed4 = new Discord.MessageEmbed()
-            .setTitle('💰 Economy')
-            .setDescription(`❌ You lost ${money} coins\n\nMultiplier: 0x`)
-            .setColor(0x8e44ad)
-            .setTimestamp()
-            .setFooter(client.config.copyright);
-         message.channel.send(moneyEmbed4);
+        return message.channel.send(embed);
     }
+
+    if (!amount) {
+        let embed = new Discord.MessageEmbed()
+            .setTitle('💰 Economy')
+            .setDescription(`❌ Specify an amount to gamble | ${client.config.prefix}roulette <color> <amount>`)
+            .setColor("#FF0000")
+            .setTimestamp()
+            .setFooter(client.config.copyright);
+        return message.channel.send(embed);
+    }
+
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [message.author.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!`);
+
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+
+                    const money = rows[0].money;
+
+                    if (amount > money) {
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`❌ You are betting more than you have`)
+                            .setColor("#FF0000")
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        return message.channel.send(embed);
+                    }
+
+                    if (color === "b" || color.includes("black")) color = 0;
+                    else if (color === "r" || color.includes("red")) color = 1;
+                    else if (color === "g" || color.includes("green")) color = 2;
+                    else {
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`❌ Specify a color | Red [1.5x] Black [2x] Green [15x]`)
+                            .setColor("#FF0000")
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        return message.channel.send(embed);
+                    }
+
+                    if (random === 0 && color === 2) {
+                        // Green
+                        amount *= 15;
+                        const add = parseInt(money) + amount;
+                        economyHelper.updateMoney(message.author.id, add, client.db);
+
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`🟩 You won ${amount} coins\n\nMultiplier: 15x`)
+                            .setColor(0x8e44ad)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(embed);
+
+                    } else if (isOdd(random) && color === 1) {
+                        // Red
+                        amount = money * 1.5;
+                        const add = parseInt(money) + parseInt(amount);
+                        economyHelper.updateMoney(message.author.id, add, client.db);
+
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`🟥 You won ${amount} coins\n\nMultiplier: 1.5x`)
+                            .setColor(0x8e44ad)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(embed);
+
+                    } else if (!isOdd(random) && color === 0) {
+                        // Black
+                        amount = money * 2;
+                        const add = parseInt(money) + parseInt(amount);
+                        economyHelper.updateMoney(message.author.id, add, client.db);
+
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`⬛ You won ${amount} coins\n\nMultiplier: 2x`)
+                            .setColor(0x8e44ad)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(embed);
+
+                    } else {
+                        // Wrong
+                        const remove = parseInt(money) - parseInt(amount);
+                        economyHelper.updateMoney(message.author.id, remove, client.db);
+
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`❌ You lost ${amount} coins\n\nMultiplier: 0x`)
+                            .setColor(0x8e44ad)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(embed);
+                    }
+                });
+            });
+        } else {
+            // User exists in DB
+
+            client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+
+                const money = rows[0].money;
+
+                if (amount > money) {
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`❌ You are betting more than you have`)
+                        .setColor("#FF0000")
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    return message.channel.send(embed);
+                }
+
+                if (color === "b" || color.includes("black")) color = 0;
+                else if (color === "r" || color.includes("red")) color = 1;
+                else if (color === "g" || color.includes("green")) color = 2;
+                else {
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`❌ Specify a color | Red [1.5x] Black [2x] Green [15x]`)
+                        .setColor("#FF0000")
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    return message.channel.send(embed);
+                }
+
+                if (random === 0 && color === 2) {
+                    // Green
+                    amount *= 15;
+                    const add = parseInt(money) + amount;
+                    economyHelper.updateMoney(message.author.id, add, client.db);
+
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`🟩 You won ${amount} coins\n\nMultiplier: 15x`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+
+                } else if (isOdd(random) && color === 1) {
+                    // Red
+                    amount = money * 1.5;
+                    const add = parseInt(money) + parseInt(amount);
+                    economyHelper.updateMoney(message.author.id, add, client.db);
+
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`🟥 You won ${amount} coins\n\nMultiplier: 1.5x`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+
+                } else if (!isOdd(random) && color === 0) {
+                    // Black
+                    amount = money * 2;
+                    const add = parseInt(money) + parseInt(amount);
+                    economyHelper.updateMoney(message.author.id, add, client.db);
+
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`⬛ You won ${amount} coins\n\nMultiplier: 2x`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+
+                } else {
+                    // Wrong
+                    const remove = parseInt(money) - parseInt(amount);
+                    economyHelper.updateMoney(message.author.id, remove, client.db);
+
+                    let embed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`❌ You lost ${amount} coins\n\nMultiplier: 0x`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(embed);
+                }
+            });
+        }
+    });
 };
 
   
