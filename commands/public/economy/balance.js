f6a240af5f49726453a207c4842643dec7bead57
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:05:28 +0200

    Economy: Updated balance Command to MySQL

--- commands/public/economy/balance.js
+++ commands/public/economy/balance.js
@@ -3,30 +3,62 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const Discord = require("discord.js");
-const economyHelper = require('./../../../utils/helper/economyHelper');
-const errorHandler = require('./../../../utils/handler/error');
+const Discord       = require("discord.js");
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
 module.exports.run = async (client, message, args) => {
 
     let user = message.mentions.members.first() || message.author;
-    await economyHelper.checkUser(user.id, client.db);
 
-    client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
-        if(error) return errorHandler.mysql(`Error while querying the money for User : "${user_id}"!\n ${error}`);
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [message.author.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!`);
 
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying the money for User : "${user.id}"!\n ${error}`);
+                    let balance = 0;
+                    let bank = 0;
 
-        const balance = rows[0].money;
-        const bank = rows[0].bank;
+                    if(rows.length > 0) {
+                        balance = rows[0].money;
+                        bank = rows[0].bank;
+                    }
 
-        let moneyEmbed = new Discord.MessageEmbed()
-            .setTitle('💰 Economy')
-            .setDescription(`**${user}'s Balance**\n\nPocket: ${balance}\nBank: ${bank}`)
-            .setColor(0x8e44ad)
-            .setTimestamp()
-            .setFooter(client.config.copyright);
-        message.channel.send(moneyEmbed);
+                    let moneyEmbed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`**${user}'s Balance**\n\nPocket: ${balance}\nBank: ${bank}`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(moneyEmbed);
 
+                });
+            });
+        } else {
+            // User exists in DB
+            client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                if (error) return errorHandler.mysql(`Error while querying the money for User : "${user.id}"!\n ${error}`);
+                let balance = 0;
+                let bank = 0;
+
+                if(rows.length > 0) {
+                    balance = rows[0].money;
+                    bank = rows[0].bank;
+                }
+
+                let moneyEmbed = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setDescription(`**${user}'s Balance**\n\nPocket: ${balance}\nBank: ${bank}`)
+                    .setColor(0x8e44ad)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+                message.channel.send(moneyEmbed);
+
+            });
+        }
     });
 };
 
