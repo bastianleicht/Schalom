528d02a70aa4b476fc94505dc244f3a84cbf81ea
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:12:02 +0200

    Economy: Updated pay Command to MySQL

--- commands/public/economy/pay.js
+++ commands/public/economy/pay.js
@@ -3,75 +3,174 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-//jshint esversion: 8
-const Discord = require("discord.js");
-const db = require("quick.db");
+const Discord       = require("discord.js");
+const economyHelper = require(__BASE__ + '/utils/helper/economyHelper');
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
 module.exports.run = async (client, message, args) => {
 
-  let user = message.mentions.members.first();
-  let member = db.fetch(`money_${message.guild.id}_${message.author.id}`);
+    let user = message.mentions.members.first();
 
-  let embed1 = new Discord.MessageEmbed()
-  .setTitle('💰 Economy')
-  .setColor("#FFFFFF")
-  .setDescription(`❌ Mention someone to pay`)
-  .setTimestamp()
-  .setFooter(client.config.copyright);
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [message.author.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!`);
 
-  if (!user) {
-      return message.channel.send(embed1);
-  }
-  let embed2 = new Discord.MessageEmbed()
-  .setTitle('💰 Economy')
-  .setColor("#FFFFFF")
-  .setDescription(`❌ Specify an amount to pay`)
-  .setTimestamp()
-  .setFooter(client.config.copyright);
-  
-  if (!args[1]) {
-      return message.channel.send(embed2);
-  }
-  let embed3 = new Discord.MessageEmbed()
-  .setTitle('💰 Economy')
-  .setColor("#FFFFFF")
-  .setDescription(`❌ You can't pay someone negative money`)
-  .setTimestamp()
-  .setFooter(client.config.copyright);
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
 
-  if (message.content.includes('-')) { 
-      return message.channel.send(embed3);
-  }
-  let embed4 = new Discord.MessageEmbed()
-  .setTitle('💰 Economy')
-  .setColor("#FFFFFF")
-  .setDescription(`❌ You don't have that much money`)
-  .setTimestamp()
-  .setFooter(client.config.copyright);
+                    const money = rows[0].money;
 
-  if (member < args[1]) {
-      return message.channel.send(embed4);
-  }
+                    let embed1 = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setColor("#FFFFFF")
+                        .setDescription(`❌ Mention someone to pay`)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
 
-  let embed5 = new Discord.MessageEmbed()
-  .setTitle('💰 Economy')
-  .setColor("#FFFFFF")
-  .setDescription(`✅ You have payed ${user.user.username} ${args[1]} coins`)
-  .setTimestamp()
-  .setFooter(client.config.copyright);
+                    if (!user) {
+                        return message.channel.send(embed1);
+                    }
+                    let embed2 = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setColor("#FFFFFF")
+                        .setDescription(`❌ Specify an amount to pay`)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
 
-  message.channel.send(embed5);
-  db.add(`money_${message.guild.id}_${user.id}`, args[1]);
-  db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1]);
+                    if (!args[1]) {
+                        return message.channel.send(embed2);
+                    }
+                    let embed3 = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setColor("#FFFFFF")
+                        .setDescription(`❌ You can't pay someone negative money`)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+
+                    if (message.content.includes('-')) {
+                        return message.channel.send(embed3);
+                    }
+                    let embed4 = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setColor("#FFFFFF")
+                        .setDescription(`❌ You don't have that much money`)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+
+                    if (money < args[1]) {
+                        return message.channel.send(embed4);
+                    }
+
+                    let embed5 = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setColor("#FFFFFF")
+                        .setDescription(`✅ You have payed ${user.user.username} ${args[1]} coins`)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+
+                    message.channel.send(embed5);
+
+                    client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                        if (error) return errorHandler.mysql(`Error while querying data for User : "${user.id}"!\n ${error}`);
+
+                        const money = rows[0].money;
+
+                        const add = parseInt(money) + args[1];
+                        economyHelper.updateMoney(user.id, add, client.db);
+
+                    });
+
+                    const remove = parseInt(money) - args[1];
+                    economyHelper.updateMoney(message.author.id, remove, client.db);
+
+                });
+            });
+        } else {
+            // User exists in DB
+
+            client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+
+                const money = rows[0].money;
+
+                let embed1 = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setColor("#FFFFFF")
+                    .setDescription(`❌ Mention someone to pay`)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+
+                if (!user) {
+                    return message.channel.send(embed1);
+                }
+                let embed2 = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setColor("#FFFFFF")
+                    .setDescription(`❌ Specify an amount to pay`)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+
+                if (!args[1]) {
+                    return message.channel.send(embed2);
+                }
+                let embed3 = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setColor("#FFFFFF")
+                    .setDescription(`❌ You can't pay someone negative money`)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+
+                if (message.content.includes('-')) {
+                    return message.channel.send(embed3);
+                }
+                let embed4 = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setColor("#FFFFFF")
+                    .setDescription(`❌ You don't have that much money`)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+
+                if (money < args[1]) {
+                    return message.channel.send(embed4);
+                }
+
+                let embed5 = new Discord.MessageEmbed()
+                    .setTitle('💰 Economy')
+                    .setColor("#FFFFFF")
+                    .setDescription(`✅ You have payed ${user.user.username} ${args[1]} coins`)
+                    .setTimestamp()
+                    .setFooter(client.config.copyright);
+
+                message.channel.send(embed5);
+
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [user.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${user.id}"!\n ${error}`);
+
+                    const money = rows[0].money;
+
+                    const add = parseInt(money) + args[1];
+                    economyHelper.updateMoney(user.id, add, client.db);
+
+                });
+
+                const remove = parseInt(money) - args[1];
+                economyHelper.updateMoney(message.author.id, remove, client.db);
+
+            });
+        }
+    });
 
 };
 
 module.exports.help = {
-  name: "pay",
-  usage: "pay <user> <amount>",
-  description: "Pays someone the specified amount.",
-  permissions: "",
-  guildOnly: false,
-  nsfw: false,
-  ownerOnly: false,
+    name: "pay",
+    usage: "pay <user> <amount>",
+    description: "Pays someone the specified amount.",
+    permissions: "",
+    guildOnly: false,
+    nsfw: false,
+    ownerOnly: false,
 };
