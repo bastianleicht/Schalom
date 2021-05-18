bc3e87d642dcecafb153bad33906c15982777680
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:11:01 +0200

    Economy: Updated weekly Command to MySQL

--- commands/public/economy/weekly.js
+++ commands/public/economy/weekly.js
@@ -3,44 +3,87 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const Discord = require("discord.js");
-const economyHelper = require('./../../../utils/helper/economyHelper');
-const errorHandler = require('./../../../utils/handler/error');
-const ms = require("parse-ms");
+const Discord       = require("discord.js");
+const ms            = require("parse-ms");
+const economyHelper = require(__BASE__ + '/utils/helper/economyHelper');
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
 module.exports.run = async (client, message, args) => {
 
     let timeout = 604800000;
     let amount = 500;
 
-    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
-        if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [message.author.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!`);
 
-        const weekly = rows[0].weekly;
-        const money = rows[0].money;
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                    if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
 
-        if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
-            let time = ms(timeout - (Date.now() - weekly));
-            let timeEmbed = new Discord.MessageEmbed()
-                .setTitle('💰 Economy')
-                .setDescription(`❌ You have already collected your weekly reward!\n\nCollect it again in ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s.`)
-                .setColor("#FF0000")
-                .setTimestamp()
-                .setFooter(client.config.copyright);
-            message.channel.send(timeEmbed);
+                    const weekly = rows[0].weekly;
+                    const money = rows[0].money;
 
+                    if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
+                        let time = ms(timeout - (Date.now() - weekly));
+                        let timeEmbed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`❌ You have already collected your weekly reward!\n\nCollect it again in ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s.`)
+                            .setColor("#FF0000")
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(timeEmbed);
+
+                    } else {
+                        let moneyEmbed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`✅ You've collected your weekly reward of ${amount} coins!`)
+                            .setColor(0x8e44ad)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(moneyEmbed);
+
+                        const add = parseInt(money) + amount;
+                        economyHelper.updateMoney(message.author.id, add, client.db);
+                        economyHelper.setValue(message.author.id, 'weekly', Date.now(), client.db);
+                    }
+                });
+            });
         } else {
-            let moneyEmbed = new Discord.MessageEmbed()
-                .setTitle('💰 Economy')
-                .setDescription(`✅ You've collected your weekly reward of ${amount} coins!`)
-                .setColor(0x8e44ad)
-                .setTimestamp()
-                .setFooter(client.config.copyright);
-            message.channel.send(moneyEmbed);
+            // User exists in DB
 
-            const add = parseInt(money) + amount;
-            economyHelper.updateMoney(message.author.id, add, client.db);
-            economyHelper.setValue(message.author.id, 'weekly', Date.now(), client.db);
+            client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+
+                const weekly = rows[0].weekly;
+                const money = rows[0].money;
+
+                if (weekly !== null && timeout - (Date.now() - weekly) > 0) {
+                    let time = ms(timeout - (Date.now() - weekly));
+                    let timeEmbed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`❌ You have already collected your weekly reward!\n\nCollect it again in ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s.`)
+                        .setColor("#FF0000")
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(timeEmbed);
+
+                } else {
+                    let moneyEmbed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`✅ You've collected your weekly reward of ${amount} coins!`)
+                        .setColor(0x8e44ad)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(moneyEmbed);
+
+                    const add = parseInt(money) + amount;
+                    economyHelper.updateMoney(message.author.id, add, client.db);
+                    economyHelper.setValue(message.author.id, 'weekly', Date.now(), client.db);
+                }
+            });
         }
     });
 };
