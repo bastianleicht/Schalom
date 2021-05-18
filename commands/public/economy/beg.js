46eb51dd2310b8af5de20ae13d60a57406a2567e
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-18 09:13:29 +0200

    Economy: Updated beg Command to MySQL

--- commands/public/economy/beg.js
+++ commands/public/economy/beg.js
@@ -3,47 +3,91 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-const Discord = require("discord.js");
-const economyHelper = require('./../../../utils/helper/economyHelper');
-const errorHandler = require('./../../../utils/handler/error');
-const ms = require("parse-ms");
+const Discord       = require("discord.js");
+const economyHelper = require(__BASE__ + '/utils/helper/economyHelper');
+const errorHandler  = require(__BASE__ + '/utils/handler/error');
+const ms            = require("parse-ms");
 
 module.exports.run = async (client, message, args) => {
-    await economyHelper.checkUser(message.author.id, client.db);
 
-    const timeout = 900000;     // 15min
-    const max_amount = 10;      //
+    const timeout = 900000;     //  15min
+    const max_amount = 10;      //  Max amount to get when Begging
     const amount = (Math.random() * (max_amount - 1) + 1).toFixed(0);
 
-    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
-        if(error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
+    client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], (error, { length }) => {
+        if (error || !length) {
+            // User does not exist in DB
+            client.db.query(`INSERT INTO economy (userid) values (?)`, [message.author.id], (error, {insertId}) => {
+                if (error) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!\n ${error}`);
+                if (!insertId) return errorHandler.mysql(`Error while inserting User : "${message.author.id}"!`);
 
-        const beg = rows[0].beg;
-        const money = rows[0].money;
+                client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                    if(error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
 
-        if (beg !== null && timeout - (Date.now() - beg) > 0) {
-            let time = ms(timeout - (Date.now() - beg));
+                    const beg = rows[0].beg;
+                    const money = rows[0].money;
 
-            let timeEmbed = new Discord.MessageEmbed()
-                .setTitle('💰 Economy')
-                .setDescription(`❌ You've already begged recently!\n\nBeg again in ${time.minutes}m ${time.seconds}s `)
-                .setColor('#FF0000')
-                .setTimestamp()
-                .setFooter(client.config.copyright);
-            message.channel.send(timeEmbed);
+                    if (beg !== null && timeout - (Date.now() - beg) > 0) {
+                        let time = ms(timeout - (Date.now() - beg));
+
+                        let timeEmbed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`❌ You've already begged recently!\n\nBeg again in ${time.minutes}m ${time.seconds}s `)
+                            .setColor('#FF0000')
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(timeEmbed);
+                    } else {
+
+                        let moneyEmbed = new Discord.MessageEmbed()
+                            .setTitle('💰 Economy')
+                            .setDescription(`✅ You've begged and received ${Number(amount)} coins`)
+                            .setColor(3447003)
+                            .setTimestamp()
+                            .setFooter(client.config.copyright);
+                        message.channel.send(moneyEmbed);
+
+                        const add = parseInt(money) + parseInt(amount);
+                        economyHelper.updateMoney(message.author.id, add, client.db);
+                        economyHelper.setValue(message.author.id, 'beg', Date.now(), client.db);
+                    }
+
+                });
+
+            });
         } else {
+            // User exists in DB
+            client.db.query('SELECT * FROM economy WHERE userID = ?', [message.author.id], function (error, rows) {
+                if(error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
 
-            let moneyEmbed = new Discord.MessageEmbed()
-                .setTitle('💰 Economy')
-                .setDescription(`✅ You've begged and received ${Number(amount)} coins`)
-                .setColor(3447003)
-                .setTimestamp()
-                .setFooter(client.config.copyright);
-            message.channel.send(moneyEmbed);
+                const beg = rows[0].beg;
+                const money = rows[0].money;
 
-            const add = parseInt(money) + parseInt(amount);
-            economyHelper.updateMoney(message.author.id, add, client.db);
-            economyHelper.setValue(message.author.id, 'beg', Date.now(), client.db);
+                if (beg !== null && timeout - (Date.now() - beg) > 0) {
+                    let time = ms(timeout - (Date.now() - beg));
+
+                    let timeEmbed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`❌ You've already begged recently!\n\nBeg again in ${time.minutes}m ${time.seconds}s `)
+                        .setColor('#FF0000')
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(timeEmbed);
+                } else {
+
+                    let moneyEmbed = new Discord.MessageEmbed()
+                        .setTitle('💰 Economy')
+                        .setDescription(`✅ You've begged and received ${Number(amount)} coins`)
+                        .setColor(3447003)
+                        .setTimestamp()
+                        .setFooter(client.config.copyright);
+                    message.channel.send(moneyEmbed);
+
+                    const add = parseInt(money) + parseInt(amount);
+                    economyHelper.updateMoney(message.author.id, add, client.db);
+                    economyHelper.setValue(message.author.id, 'beg', Date.now(), client.db);
+                }
+            });
         }
 
     });
