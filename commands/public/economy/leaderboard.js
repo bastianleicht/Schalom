cc7164b6291686fa64e03759c2f3510bd91654b7
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-20 21:47:44 +0200

    Economy: Updated leaderboard Command to have a dynamic board :^)

--- commands/public/economy/leaderboard.js
+++ commands/public/economy/leaderboard.js
@@ -6,28 +6,19 @@
 const Discord       = require('discord.js');
 const errorHandler  = require(__BASE__ + '/utils/handler/error');
 
-//TODO: Need to fix the Embed because it crashes the Bot when there are not 10 Users in the DB!
-
 module.exports.run = async (client, message, args) => {
-
     client.db.query('SELECT * FROM economy ORDER BY money DESC LIMIT 10', [message.author.id], function (error, rows) {
         if (error) return errorHandler.mysql(`Error while querying data for User : "${message.author.id}"!\n ${error}`);
 
+        let board = '';
+        Object.keys(rows).forEach(function (index) {
+            const row = rows[index];
+            board += `** ${parseInt(index) + 1}. ** <@${row.userID}> (${row.money} Coins)\n`;
+        });
+
         let embed = new Discord.MessageEmbed()
             .setTitle('💰 Economy')
-            .addField('Leaderboard:', [
-                `** 1. ** <@${rows[0].userID}> (${rows[0].money} Coins)`,
-                `** 2. ** <@${rows[1].userID}> (${rows[1].money} Coins)`,
-                `** 3. ** <@${rows[2].userID}> (${rows[2].money} Coins)`,
-                `** 4. ** <@${rows[3].userID}> (${rows[3].money} Coins)`,
-                `** 5. ** <@${rows[4].userID}> (${rows[4].money} Coins)`,
-                `** 6. ** <@${rows[5].userID}> (${rows[5].money} Coins)`,
-                `** 7. ** <@${rows[6].userID}> (${rows[6].money} Coins)`,
-                `** 8. ** <@${rows[7].userID}> (${rows[7].money} Coins)`,
-                `** 9. ** <@${rows[8].userID}> (${rows[8].money} Coins)`,
-                `** 10. ** <@${rows[9].userID}> (${rows[9].money} Coins)`,
-                '\u200b'
-            ])
+            .addField('Leaderboard:', board + '\u200b')
             .setColor(0x8e44ad)
             .setTimestamp()
             .setFooter(client.config.copyright);
