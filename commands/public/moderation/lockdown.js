e53a94e67f0ea04c6a5d0072a7d142d259f65eb1
routerabfrage <50463419+routerabfrage@users.noreply.github.com>
2021-05-23 20:17:35 +0200

    Moderation: Updated and fixed the lockdown command

--- commands/public/moderation/lockdown.js
+++ commands/public/moderation/lockdown.js
@@ -3,79 +3,83 @@
  *
  * PDX-License-Identifier: BSD-2-Clause
  */
-// jshint esversion: 8
 const Discord = require('discord.js');
-const ms = require("ms");
-const config = require('../../../opt/config.json');
+const ms = require('ms');
+
+// TODO: Update to seconds instead of milliseconds.
 
 module.exports.run = async (client, message, args) => {
-    //TODO Working
-    if (message.channel === 'dm') return message.channel.send('This Command is only available in Guilds!');
+    if (message.channel === 'dm') return;
 
     if (message.member.hasPermission("MANAGE_GUILD")) {
+        if (!client.lockit) client.lockit = [];
 
-        // if (!client.lockit) client.lockit = [];
+        let time = args.join(' ');
+        let validUnlocks = ['release', 'unlock'];
+        if (!time) {
+            let embed = new Discord.MessageEmbed()
+                .setTitle('Error')
+                .setDescription("👾 You must set a duration for the lockdown in either hours, minutes or seconds")
+                .setColor('36393e');
+            return message.channel.send(embed);
+        }
 
-        // let time = args.join(' ');
-        // let validUnlocks = ['release', 'unlock'];
-        // var notimeembed = new Discord.MessageEmbed()
-        //     .setTitle('Error')
-        //     .setDescription("👾 You must set a duration for the lockdown in either hours, minutes or seconds")
-        //     .setColor('36393e');
-        // if (!time) return message.channel.send(notimeembed);
-
-        // if (validUnlocks.includes(time)) {
-        //     message.channel.overwritePermissions(message.guild.id, {
-        //         SEND_MESSAGES: null
-        //     }).then(() => {
-        //         var liftedembed = new Discord.MessageEmbed()
-        //             .setTitle('🔒 Lockdown')
-        //             .setDescription("🔓 Loockdown lifted.")
-        //             .setColor('36393e');
-        //         message.channel.send(liftedembed);
-        //         clearTimeout(client.lockit[message.channel.id]);
-        //         delete client.lockit[message.channel.id];
-        //     }).catch(error => {
-        //         console.log(error);
-        //     });
-        // } else {
-        //     message.channel.overwritePermissions(message.guild.id, {
-        //         SEND_MESSAGES: false
-        //     }).then(() => {
-        //         var lockdownembed = new Discord.MessageEmbed()
-        //             .setTitle("🔒 Channel Locked")
-        //             .addField("Locked by", `${message.author}`, true)
-        //             .addField("Locked for", `${ms(ms(time), { long:true })}`, true)
-        //             .setColor('36393e');
-        //         message.channel.send(lockdownembed).then(() => {
-
-        //             client.lockit[message.channel.id] = setTimeout(() => {
-        //                 var liftedembed = new Discord.MessageEmbed()
-        //                     .setTitle('🔒 Lockdown')
-        //                     .setDescription("🔓 Loockdown lifted.")
-        //                     .setColor('36393e');
-        //                 message.channel.overwritePermissions(message.guild.id, {
-        //                     SEND_MESSAGES: null
-        //                 }).then(
-
-        //                     message.channel.send(liftedembed)).catch(console.error);
-        //                 delete client.lockit[message.channel.id];
-        //             }, ms(time));
-
-        //         }).catch(error => {
-        //             console.log(error);
-        //         });
-        //     });
-        // }
-
+        if (validUnlocks.includes(time)) {
+            message.channel.overwritePermissions([
+                {
+                    id: message.guild.id,
+                    allow: ['SEND_MESSAGES'],
+                }
+            ]).then(() => {
+                let embed = new Discord.MessageEmbed()
+                    .setTitle('🔒 Lockdown')
+                    .setDescription("🔓 Lockdown lifted.")
+                    .setColor('36393e');
+                message.channel.send(embed);
+                clearTimeout(client.lockit[message.channel.id]);
+                delete client.lockit[message.channel.id];
+            }).catch(error => {
+                console.log(error);
+            });
+        } else {
+            message.channel.overwritePermissions( [
+                {
+                    id: message.guild.id,
+                    deny: ['SEND_MESSAGES'],
+                }
+                ]).then(() => {
+                let embed = new Discord.MessageEmbed()
+                    .setTitle("🔒 Channel Locked")
+                    .addField("Locked by", `${message.author}`, true)
+                    .addField("Locked for", `${ms(ms(time), { long:true })}`, true)
+                    .setColor('36393e');
+                message.channel.send(embed).then(() => {
+                    client.lockit[message.channel.id] = setTimeout(() => {
+                        let embed = new Discord.MessageEmbed()
+                            .setTitle('🔒 Lockdown')
+                            .setDescription("🔓 Lockdown lifted.")
+                            .setColor('36393e');
+                        message.channel.overwritePermissions([
+                            {
+                                id: message.guild.id,
+                                allow: ['SEND_MESSAGES'],
+                            }
+                        ]).then(
+                            message.channel.send(embed)).catch(console.error);
+                        delete client.lockit[message.channel.id];
+                    }, ms(time));
+                }).catch(error => {
+                    console.log(error);
+                });
+            });
+        }
     } else {
-        var nopermsembed = new Discord.MessageEmbed()
+        let embed = new Discord.MessageEmbed()
             .setTitle('Error')
             .setDescription("👾 Missing Permissions :: MANAGE_SERVER")
             .setColor('36393e');
-        message.channel.send(nopermsembed);
+        message.channel.send(embed);
     }
-
 };
 
 module.exports.help = {
